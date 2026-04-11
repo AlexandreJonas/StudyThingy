6a9550da64f8f7dc361ee4e083c6973a33ed83fb
Alexandre Jonas <jonassouzafonseca@gmail.com>
2026-04-11 19:05:28 -0300

    Adding base react hook form implementation

--- rhf-demo/src/components/YoutubeForm.tsx
+++ rhf-demo/src/components/YoutubeForm.tsx
@@ -1,20 +1,30 @@
-import React from "react";
+import { useForm } from "react-hook-form"
+import {DevTool} from "@hookform/devtools"
 
 export const YoutubeForm = () => {
+
+    const form = useForm()
+    const {register, control} = form
+    // const {name, ref, onChange, onBlur} = register("username")
+
     return(
         <div>
             <form>
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username"></input>
+                {/* <label htmlFor="username">Username</label>
+                <input type="text" id="username" name={name} ref={ref} onChange={onChange} onBlur={onBlur}></input> */}
 
-                <label htmlFor="emal">Email</label>
-                <input type="email" id="emal" name="emal"></input>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" {...register("username")}></input>
+
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" {...register("email")}></input>
 
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" name="channel"></input>
+                <input type="text" id="channel" {...register("channel")}></input>
 
                 <button>Submit</button>
             </form>
+            <DevTool control={control}/>
         </div>
     )
 }
